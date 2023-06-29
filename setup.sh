53cfe2bcb72743158ab890e7f6f6e4400e921a99
eterna <eterna@localhost.localdomain>
2023-06-29 17:23:59 +0300

    “eternal”

--- setup.sh
+++
@@ -1,5 +0,0 @@
-echo welcome to the dotfiles installation.    
-sudo tar -xvf Tela-circle-nord.tar.xz -C /usr/share/icons
-sudo tar -xvf Nordic-darker.tar.xz -C /usr/share/themes && sudo tar -xvf Nordic-darker-standard-buttons.tar.xz -C /usr/share/themes
-dconf load /org/gnome/ < gnome-backup
-
